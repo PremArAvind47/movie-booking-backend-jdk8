e8748084cf3fb1f2e9c0e17418303b0cbd5de55d
Prem Aravind <bluescope.prem@godrejproperties.com>
2026-09-09 16:07:12 +0530

    Add GET /hello endpoint

---
+++ src/main/java/com/example/moviebooking/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.moviebooking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, DevOps!";
+    }
+
+}
